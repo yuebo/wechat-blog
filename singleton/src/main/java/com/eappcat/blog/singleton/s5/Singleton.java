5a6abe05df35d02109c8c4ce995b5ca6b27b874a
Xuan Yue Bo <317728991@qq.com>
2019-03-24 07:26:45 +0800

    add JDK8 interface

--- singleton/src/main/java/com/eappcat/blog/singleton/s5/Singleton.java
+++ singleton/src/main/java/com/eappcat/blog/singleton/s5/Singleton.java
@@ -4,12 +4,12 @@ package com.eappcat.blog.singleton.s5;
  * 静态内部类
  */
 public class Singleton {
-    private static class Inner{
+    private static class SingleHolder{
         private final static Singleton instance=new Singleton();
     }
     private Singleton(){
     }
     public static Singleton getInstance(){
-        return Inner.instance;
+        return SingleHolder.instance;
     }
 }
